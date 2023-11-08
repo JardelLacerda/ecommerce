470da0dee992da211a5aa0745a7c21718e7c7820
Jardel Lacerda <jardellacerda2001@gmail.com.br>
2023-11-08 13:51:17 -0300

    fix: add files for merge

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from "./users.routers";
 import sessionRouter from "./session.routers";
+import storesRouter from "./stores.router";
 
 const mainRouter = Router()
 
 mainRouter.use("/users", userRouter)
 mainRouter.use("/login", sessionRouter)
+mainRouter.use("/stores", storesRouter)
 
 export default mainRouter
---
+++ backend/src/schemas/stores.schemas.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const storesSchema = z.object({
+    id: z.string(),
+    title: z.string(),
+    description: z.string().nullish(),
+    logo: z.string().url()
+})
+
+export const storeCreateSchema = storesSchema.omit({
+    id: true
+})
+
+export const sotreUpdateSchema = storeCreateSchema.partial()
